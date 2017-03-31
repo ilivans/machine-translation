99ca0d92b0ee52839b726c8a6e8fe595c0f865e4
Ilya Ivanov <ilya.ivanov.se@gmail.com>
2017-03-31 16:42:22 +0300

    basic model (ibm 1)

--- data_driven/assignment1/models.py
+++ data_driven/assignment1/models.py
@@ -5,26 +5,13 @@ class TranslationModel:
     "Models conditional distribution over trg words given a src word."
 
     def __init__(self, src_corpus, trg_corpus):
-        self._src_trg_counts = {} # Statistics
+        self._src_trg_counts = defaultdict(lambda : {}) # Statistics
         self._trg_given_src_probs = {} # Parameters
-        # for src_sentence, trg_sentence in zip(src_corpus, trg_corpus):
-        #     for src_token in src_sentence:
-        #         if src_token not in self._src_trg_counts:
-        #             self._src_trg_counts = {}
-        #         for trg_token in trg_sentence:
-        #             self._src_trg_counts[src_token][trg_token] = self._src_trg_counts[src_token].get(trg_token, 0) + 1
-        # for src_token, trg_counts in self._src_trg_counts.iteritems():
-        #     trgs_count_total = float(sum(trg_counts.itervalues()))
-        #     self._trg_given_src_probs[src_token] = {}
-        #     for trg_token, trg_count in trg_counts:
-        #         self._trg_given_src_probs[src_token][trg_token] = trg_count / trgs_count_total
 
     def get_conditional_prob(self, src_token, trg_token):
         "Return the conditional probability of trg_token given src_token."
-        if src_token not in self._trg_given_src_probs:
-            return 0.0
-        if trg_token not in self._trg_given_src_probs[src_token]:
-            return 0.0
+        if self._trg_given_src_probs == {}:
+            return 1.0
         return self._trg_given_src_probs[src_token][trg_token]
 
     def collect_statistics(self, src_tokens, trg_tokens, posterior_matrix):
@@ -32,11 +19,19 @@ class TranslationModel:
         assert len(posterior_matrix) == len(trg_tokens)
         for posterior in posterior_matrix:
             assert len(posterior) == len(src_tokens)
-        assert False, "Collect statistics here in self._src_trg_counts"
+
+        for trg_token, posterior_probs in zip(trg_tokens, posterior_matrix):
+            for src_token, posterior_prob in zip(src_tokens, posterior_probs):
+                self._src_trg_counts[src_token][trg_token] = self._src_trg_counts[src_token].get(trg_token, 0.0) + posterior_prob
 
     def recompute_parameters(self):
         "Reestimate parameters from counts then reset counters"
-        assert False, "Recompute parameters in self._trg_given_src_probs here."
+        for src_token, trg_counts in self._src_trg_counts.iteritems():
+            trgs_count_total = float(sum(trg_counts.itervalues()))
+            self._trg_given_src_probs[src_token] = {}
+            for trg_token, trg_count in trg_counts.iteritems():
+                self._trg_given_src_probs[src_token][trg_token] = trg_count / trgs_count_total
+        self._src_trg_counts = defaultdict(lambda: {})
 
 
 class PriorModel:
--- data_driven/assignment1/word_alignment.py
+++ data_driven/assignment1/word_alignment.py
@@ -4,50 +4,77 @@ from math import log
 from models import PriorModel
 from models import TranslationModel
 
+
 def get_posterior_distribution_for_trg_token(trg_index, src_tokens, trg_tokens,
                                              prior_model, translation_model):
     "Compute the posterior distribution over alignments for trg_index: P(a_j = i|f_j, e)."
-    # assert False, "Implement this."
     # marginal_prob = p(f_j|e)
     # posterior_probs[i] = p(a_j = i|f_j, e)
-    src_len, trg_len = len(src_tokens), len(trg_tokens)
     trg_token = trg_tokens[trg_index]
-    posterior_probs = [0.0] * len(src_tokens)
-    for i, src_token in enumerate(src_tokens):
-        prior_prob = prior_model.get_prior_prob(i, trg_index, src_len, trg_len)
+    src_len, trg_len = len(src_tokens), len(trg_tokens)
+
+    conditional_probs = []
+    for src_token in src_tokens:
         conditional_prob = translation_model.get_conditional_prob(src_token, trg_token)
-        posterior_probs[i] = prior_prob * conditional_prob
+        conditional_probs.append(conditional_prob)
+    sum_cond_probs = sum(conditional_probs)
+
+    posterior_probs = []
+    marginal_prob = 0.0
+    for src_index, src_token in enumerate(src_tokens):
+        conditional_prob = conditional_probs[src_index]
+        posterior_prob = conditional_prob / sum_cond_probs # if sum_cond_probs != 0.0 else 0.0
+        posterior_probs.append(posterior_prob)
+        prior_prob = prior_model.get_prior_prob(src_index, trg_index, src_len, trg_len)
+        marginal_prob += prior_prob * conditional_prob
+
     return marginal_prob, posterior_probs
+
 
 def get_posterior_alignment_matrix(src_tokens, trg_tokens, prior_model, translation_model):
     "For each target token compute the posterior alignment probability: p(a_j=i | f_j, e)"
     # 1. Prior model assumes that a_j is independent of all other alignments.
     # 2. Translation model assume each target word is generated independently given its alignment.
-    posterior_matrix = []
     sentence_marginal_log_likelihood = 0.0
-    assert False, "Implement this."
+    posterior_matrix = []
+    for trg_index in xrange(len(trg_tokens)):
+        marginal_prob, posterior_probs = get_posterior_distribution_for_trg_token(trg_index, src_tokens, trg_tokens,
+                                                                                  prior_model, translation_model)
+        sentence_marginal_log_likelihood += log(marginal_prob) # if marginal_prob != 0.0 else 0.0
+        posterior_matrix.append(posterior_probs)
+
     return sentence_marginal_log_likelihood, posterior_matrix
+
 
 def collect_expected_statistics(src_corpus, trg_corpus, prior_model, translation_model):
     "Infer posterior distribution over each sentence pair and collect statistics: E-step"
     corpus_marginal_log_likelihood = 0.0
-    assert False, "Implement this."
-    # 1. Infer posterior
+    for src_tokens, trg_tokens in zip(src_corpus, trg_corpus):
+        src_len, trg_len = len(src_tokens), len(trg_tokens)
+        # 1. Infer posterior
+        sentence_marginal_log_likelihood, posterior_matrix = get_posterior_alignment_matrix(
+            src_tokens, trg_tokens, prior_model, translation_model
+        )
+        # 2. Collect statistics in each model.
+        prior_model.collect_statistics(src_len, trg_len, posterior_matrix)
+        translation_model.collect_statistics(src_tokens, trg_tokens, posterior_matrix)
+        # 3. Update log prob
+        corpus_marginal_log_likelihood += sentence_marginal_log_likelihood
 
-    # 2. Collect statistics in each model.
-
-    # 3. Update log prob
     return corpus_marginal_log_likelihood
+
 
 def reestimate_models(prior_model, translation_model):
     "Recompute parameters of each model: M-step"
     prior_model.recompute_parameters()
     translation_model.recompute_parameters()
 
+
 def initialize_models(src_corpus, trg_corpus):
     prior_model = PriorModel(src_corpus, trg_corpus)
     translation_model = TranslationModel(src_corpus, trg_corpus)
     return prior_model, translation_model
+
 
 def estimate_models(src_corpus, trg_corpus, prior_model, translation_model, num_iterations):
     "Estimate models iteratively."
@@ -59,10 +86,11 @@ def estimate_models(src_corpus, trg_corpus, prior_model, translation_model, num_
             print "corpus log likelihood: %1.3f" % corpus_log_likelihood
     return prior_model, translation_model
 
+
 def align_sentence_pair(src_tokens, trg_tokens, prior_probs, translation_probs):
     "For each target token, find the src_token with the highest posterior probability."
     # Compute the posterior distribution over alignments for all target tokens.
-    corpus_log_prob, posterior_matrix = get_posterior_alignment_matrix(
+    _, posterior_matrix = get_posterior_alignment_matrix(
         src_tokens, trg_tokens, prior_probs, translation_probs)
     # For each target word find the src index with the highest posterior probability.
     alignments = []
@@ -70,6 +98,7 @@ def align_sentence_pair(src_tokens, trg_tokens, prior_probs, translation_probs):
         best_src_index = posteriors.index(max(posteriors))
         alignments.append((best_src_index, trg_index))
     return alignments
+
 
 def align_corpus_given_models(src_corpus, trg_corpus, prior_model, translation_model):
     "Align each sentence pair in the corpus in turn."
@@ -80,6 +109,7 @@ def align_corpus_given_models(src_corpus, trg_corpus, prior_model, translation_m
         alignments.append(these_alignments)
     return alignments
 
+
 def align_corpus(src_corpus, trg_corpus, num_iterations):
     "Learn models and then align the corpus using them."
     prior_model, translation_model = initialize_models(src_corpus, trg_corpus)
@@ -87,14 +117,13 @@ def align_corpus(src_corpus, trg_corpus, num_iterations):
         src_corpus, trg_corpus, prior_model, translation_model, num_iterations)
     return align_corpus_given_models(src_corpus, trg_corpus, prior_model, translation_model)
 
+
 if __name__ == "__main__":
     if not len(sys.argv) == 5:
         print "Usage: python word_alignment.py src_corpus trg_corpus iterations output_prefix."
         sys.exit(0)
     src_corpus = utils.read_all_tokens(sys.argv[1])
     trg_corpus = utils.read_all_tokens(sys.argv[2])
-    # print src_corpus[0]
-    # sys.exit(0)
     num_iterations = int(sys.argv[3])
     output_prefix = sys.argv[4]
     assert len(src_corpus) == len(trg_corpus), "Corpora should be same size!"
